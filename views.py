f1c0abf837166bf211f41042adc821c68bd9ff63
IsamaradaSilvaFerreira <75046048+IsamaradaSilvaFerreira@users.noreply.github.com>
2021-12-09 11:33:59 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from .models import Conteudo, Prova, Simulado, Matematica, Redacao, Linux
+
+def index(request):
+    photo = Conteudo.objects.all()
+    texto = Conteudo.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'layout.html', context)
+
+def pro(request):
+    photo = Prova.objects.all()
+    texto = Prova.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'prov.html', context)
+
+def cursos(request):
+    photo = Conteudo.objects.all()
+    texto = Conteudo.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+    }
+    return render(request, 'cursoo.html', context)
+
+def conteudos(request):
+    photo = Conteudo.objects.all()
+    texto = Conteudo.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'lista.html', context)
+
+
+def simul(request):
+    photo = Simulado.objects.all()
+    texto = Simulado.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'simul.html', context)
+
+
+def Matema(request):
+    photo = Matematica.objects.all()
+    texto = Matematica.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'mate.html', context)
+
+
+def Redac(request):
+    photo = Redacao.objects.all()
+    texto = Redacao.objects.all()
+    context = {
+        'photo': photo,
+        'texto': texto,
+
+    }
+    return render(request, 'redaca.html', context)
+
+
