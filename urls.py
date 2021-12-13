5be6567330d2af437a23070a7f4856f316a41d19
IsamaradaSilvaFerreira <75046048+IsamaradaSilvaFerreira@users.noreply.github.com>
2021-12-12 21:15:12 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
 
-from django.urls import path
-
-from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from pagina import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', views.index),
     path('provas/', views.pro),
     path('conteudos/', views.conteudos),
@@ -11,13 +15,6 @@ urlpatterns = [
     path('simulados/', views.simul),
     path('disciplinas/', views.Matema),
     path('Redacaos/', views.Redac),
-]
 
 
-
-
-
-
-
-
-
+]+ static(settings.MEDIA_URL, documento_root=settings.MEDIA_ROOT)
